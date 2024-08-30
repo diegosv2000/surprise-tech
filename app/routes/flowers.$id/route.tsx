c4641a8b6d4adcf1f2c9b82110f16d1ae9234c44
dsalazarv <diegoedgardosalazarvergara@gmail.com>
2024-08-30 16:00:14 -0500

    feat: fetch data

--- app/routes/flowers.$id/route.tsx
+++ app/routes/flowers.$id/route.tsx
@@ -1,5 +1,8 @@
 import type { MetaFunction } from "@remix-run/node";
+import { unstable_defineLoader as defineLoader } from "@remix-run/node";
+
 import "./flowers.css";
+import {  useLoaderData } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -8,10 +11,34 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export const loader = defineLoader(async ({ params }) => {
+  const { id } = params;
+
+ 
+  
+
+  try{
+    console.log("este es el ID:", id )
+    const response = await fetch(`https://fakestoreapi.com/products/${id}`)
+    
+    const data = await  response.json()
+
+    return data
+  }catch(e){
+
+    throw new Error ("Hubo un error xD")
+  }
+
+  
+});
+
 export default function Index() {
+  const data = useLoaderData<{
+    id: string
+  }>();
   return (
     <div className="container-flowers">
-      <p>Flores Amarillas Por ID</p>
+      <p>Flores Amarillas Por ID: {data.id}</p>
       
     </div>
   );
