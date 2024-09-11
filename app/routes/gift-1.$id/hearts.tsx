3035ad38a2febd0e14e19a385acddb5fc4a5ec6e
dsalazarv <diegoedgardosalazarvergara@gmail.com>
2024-09-11 00:43:24 -0500

    feat: add classes

--- app/routes/gift-1.$id/hearts.tsx
+++ app/routes/gift-1.$id/hearts.tsx
@@ -34,50 +34,50 @@ export const Hearts:FC<HeartsProps> = ({text}) => {
       </div>
 
       <div className="heart-global-4">
-        <div className="heart-animated">
-          <div className="border-global"></div>
+        <div className="heart-animated-4">
+          <div className="border-global-4"></div>
           <div className="heart"></div>
         </div>
       </div>
 
       <div className="heart-global-5">
-        <div className="heart-animated">
-          <div className="border-global"></div>
+        <div className="heart-animated-5">
+          <div className="border-global-5"></div>
           <div className="heart"></div>
         </div>
       </div>
 
       <div className="heart-global-6">
-        <div className="heart-animated">
-          <div className="border-global"></div>
+        <div className="heart-animated-6">
+          <div className="border-global-6"></div>
           <div className="heart"></div>
         </div>
       </div>
 
       <div className="heart-global-7">
-        <div className="heart-animated">
-          <div className="border-global"></div>
+        <div className="heart-animated-7">
+          <div className="border-global-7"></div>
           <div className="heart"></div>
         </div>
       </div>
 
       <div className="heart-global-8">
-        <div className="heart-animated">
-          <div className="border-global"></div>
+        <div className="heart-animated-8">
+          <div className="border-global-8"></div>
           <div className="heart"></div>
         </div>
       </div>
 
       <div className="heart-global-9">
-        <div className="heart-animated">
-          <div className="border-global"></div>
+        <div className="heart-animated-9">
+          <div className="border-global-9"></div>
           <div className="heart"></div>
         </div>
       </div>
 
       <div className="heart-global-10">
-        <div className="heart-animated">
-          <div className="border-global"></div>
+        <div className="heart-animated-10">
+          <div className="border-global-10"></div>
           <div className="heart"></div>
         </div>
       </div>
